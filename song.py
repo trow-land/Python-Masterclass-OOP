1431b1024feb8aab888501bbd6a2570ac757b456
Tom Rowland <tom_rowland@outlook.com>
2023-09-30 06:38:27 +0100

    Fixed artist bug and created create_checkfile function

--- song.py
+++ song.py
@@ -95,6 +95,13 @@ class Artist:
 
 # Garbage collection -> how objects that are no longer used are delt with.
 
+def find_object(field, object_list):
+    """ check object_list to see if an object with a name equal to attribute equal to 'field' exists, return it if so"""
+    for item in object_list:
+        if item.name == field:
+            return item
+    return None
+
 
 def load_data():
     new_artist = None
@@ -106,13 +113,56 @@ def load_data():
             # data row should consist of (artist, album, year, song)
             artist_field, album_field, year_field, song_field = tuple(line.strip('\n').split('\t'))
             year_field = int(year_field)
-            print(artist_field, album_field, year_field, song_field)
+            print(artist_field, ":", album_field, ":", year_field, ":", song_field)
+
+            if new_artist is None:
+                new_artist = Artist(artist_field)
+                artist_list.append(new_artist)
+            elif new_artist.name != artist_field:
+                # We've just read details for a new artist
+                # retrieve the artist object if it exists, otherwise we create one and add to artist list
+                new_artist = find_object(artist_field, artist_list)
+                if new_artist is None:
+                    new_artist = Artist(artist_field)
+                    artist_list.append(new_artist)
+                new_album = None
+
+            if new_album is None:
+                new_album = Album(album_field, year_field, new_artist)
+                new_artist.add_album(new_album)
+            elif new_album.name != album_field:
+                # We've just read a new album for the current artist
+                # retrieve the album object if there is one, else we create it and add to artist's collection
+                new_album = find_object(album_field, new_artist.albums)
+                if new_album is None:
+                    new_album = Album(album_field, year_field, new_artist)
+                    new_artist.add_album(new_album)
+
+            # create a new song object and add it to the current albums collection
+            new_song = Song(song_field, new_artist)
+            new_album.add_song(new_song)
+
+    return artist_list
 
 
-if __name__== '__main__':
-    load_data()
+def create_checkfile(artist_list):
+    """ create a check file from the object data for comparison with the original file"""
+
+    with open("checkfile.txt", 'w') as checkfile:
+        for new_artist in artist_list:
+            for new_album in new_artist.albums:
+                for new_song in new_album.tracks:
+                    print("{0.name}\t{1.name}\t{1.year}\t{2.title}".format(new_artist, new_album, new_song),
+                          file=checkfile)
 
 
+
+if __name__ == '__main__':
+    artists = load_data()
+
+print("There are {} artists".format(len(artists)))
+
+create_checkfile(artists)
 
 
 
